8a7b590ebf330e0ee88d52aafaf196aef41f564b
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2012-03-07 19:37:40 +0100

    proba doutro slider

---
+++ public/javascripts/cursorLoading.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+(document).ready(function(){
+   $("a").mouseclick(function(event){
+      $("#wrapper").addClass("mouseWaiting");
+   });
+   $("a").mouseout(function(event){
+      $("#wrapper").removeClass("mouseNormal");
+   });
+});
+
+$(document).ready(function () {
+$("a").click(function () {
+      $("div").css("cursor","wait");
+});
+});*/
